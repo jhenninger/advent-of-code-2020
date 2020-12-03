5c79463c6ab0492e377180264da5e1004e248da6
Johannes Henninger <jhenninger@users.noreply.github.com>
2020-12-03 20:55:42 +0100

    optimize day 03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,18 +1,20 @@
 fn main() {
-    let input = include_str!("../input");
+    let lines: Vec<_> = include_str!("../input").lines().collect();
 
-    println!("Part 1: {}", trees(input, 3, 1));
+    println!("Part 1: {}", trees(&lines, 3, 1));
 
-    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-    let part_2: usize = slopes.iter().map(|&(x, y)| trees(input, x, y)).product();
+    let part_2: usize = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+        .iter()
+        .map(|&(x, y)| trees(&lines, x, y))
+        .product();
 
     println!("Part 2: {}", part_2);
 }
 
-fn trees(input: &str, dx: usize, dy: usize) -> usize {
+fn trees(lines: &[&str], dx: usize, dy: usize) -> usize {
     let mut x = 0;
-    input
-        .lines()
+    lines
+        .iter()
         .step_by(dy)
         .filter(|row| {
             let idx = x;
