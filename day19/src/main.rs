61ab269da1958aa1dd1fff3b6851c2039b183df7
Johannes Henninger <jhenninger@users.noreply.github.com>
2020-12-19 17:27:06 +0100

    use pcre2 for day 19, so much easier...

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use regex::Regex;
+use pcre2::bytes::Regex;
 
 enum Rule {
     Char(char),
@@ -35,51 +35,19 @@ fn main() {
         })
         .collect();
 
-    let msgs: Vec<_> = iter.next().unwrap().lines().collect();
+    let msgs: Vec<_> = iter.next().unwrap().lines().map(|l| l.as_bytes()).collect();
 
     println!(
         "Part 1: {}\nPart 2: {}",
-        part_1(&rules, &msgs),
-        part_2(&rules, &msgs)
+        count(&rules, msgs.as_slice(), false),
+        count(&rules, msgs.as_slice(), true)
     );
 }
 
-fn part_1(rules: &Rules, msgs: &[&str]) -> usize {
-    let regex = build_regex(rules, false);
-    msgs.iter().filter(|line| regex.is_match(line)).count()
-}
-
-fn part_2(rules: &Rules, msgs: &[&str]) -> usize {
-    let regex = build_regex(rules, true);
-
-    let mut r42 = String::new();
-    build_regex_string(&rules, &mut r42, 42, false);
-    let r42 = Regex::new(&r42).unwrap();
-
-    let mut r31 = String::new();
-    build_regex_string(&rules, &mut r31, 31, false);
-    let r31 = Regex::new(&r31).unwrap();
-
+fn count(rules: &Rules, msgs: &[&[u8]], part_2: bool) -> usize {
+    let regex = build_regex(rules, part_2);
     msgs.iter()
-        .filter(|line| {
-            regex
-                .captures(line)
-                .map(|captures| {
-                    captures.iter().skip(1).flatten().all(|m| {
-                        // m is a match of rule 11
-                        let str = m.as_str();
-                        for i in 0..str.len() {
-                            let (left, right) = str.split_at(i);
-                            // this is kinda buggy since the find_iter matches don't have to be consecutive, but it works for my input
-                            if r42.find_iter(left).count() == r31.find_iter(right).count() {
-                                return true;
-                            }
-                        }
-                        false
-                    })
-                })
-                .unwrap_or(false)
-        })
+        .filter(|line| regex.is_match(line).unwrap_or(false))
         .count()
 }
 
@@ -87,7 +55,7 @@ fn build_regex(rules: &Rules, part_2: bool) -> Regex {
     let mut s = String::from("^");
     build_regex_string(rules, &mut s, 0, part_2);
     s.push_str("$");
-    s.parse().unwrap()
+    Regex::new(&s).unwrap()
 }
 
 fn build_regex_string(rules: &Rules, s: &mut String, r: usize, part_2: bool) {
@@ -100,9 +68,9 @@ fn build_regex_string(rules: &Rules, s: &mut String, r: usize, part_2: bool) {
         if r == 11 {
             s.push_str("(");
             build_regex_string(rules, s, 42, true);
-            s.push_str("+");
+            s.push_str("(?1)?");
             build_regex_string(rules, s, 31, true);
-            s.push_str("+)");
+            s.push_str(")");
             return;
         }
     }
