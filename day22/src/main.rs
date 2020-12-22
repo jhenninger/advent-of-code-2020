33e3adf3874ca0c597081477432e316575ec01e7
Johannes Henninger <jhenninger@users.noreply.github.com>
2020-12-22 14:16:19 +0100

    only store hashes for day 22

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,10 +1,13 @@
-use std::collections::{HashSet, VecDeque};
+use std::{
+    collections::{hash_map::DefaultHasher, HashSet, VecDeque},
+    hash::{Hash, Hasher},
+};
 
 fn main() {
     let input = include_str!("../input");
 
     let mut iter = input.split("\n\n").map(|side| {
-        let stack: VecDeque<usize> = side
+        let stack: VecDeque<u8> = side
             .lines()
             .skip(1)
             .map(|line| line.parse().unwrap())
@@ -22,20 +25,20 @@ fn main() {
     );
 }
 
-fn play<F>(mut p1: VecDeque<usize>, mut p2: VecDeque<usize>, variant: F) -> usize
+fn play<F>(mut p1: VecDeque<u8>, mut p2: VecDeque<u8>, variant: F) -> usize
 where
-    F: FnOnce(&mut VecDeque<usize>, &mut VecDeque<usize>) -> bool,
+    F: FnOnce(&mut VecDeque<u8>, &mut VecDeque<u8>) -> bool,
 {
     let winner = if variant(&mut p1, &mut p2) { p1 } else { p2 };
     winner
         .iter()
         .rev()
         .enumerate()
-        .map(|(i, c)| (i + 1) * c)
+        .map(|(i, c)| (i + 1) * *c as usize)
         .sum()
 }
 
-fn normal(p1: &mut VecDeque<usize>, p2: &mut VecDeque<usize>) -> bool {
+fn normal(p1: &mut VecDeque<u8>, p2: &mut VecDeque<u8>) -> bool {
     while !p1.is_empty() && !p2.is_empty() {
         let c1 = p1.pop_front().unwrap();
         let c2 = p2.pop_front().unwrap();
@@ -52,21 +55,28 @@ fn normal(p1: &mut VecDeque<usize>, p2: &mut VecDeque<usize>) -> bool {
     p2.is_empty()
 }
 
-fn recursive(p1: &mut VecDeque<usize>, p2: &mut VecDeque<usize>) -> bool {
-    let mut cache: HashSet<(VecDeque<usize>, VecDeque<usize>)> = HashSet::new();
+fn hash(p1: &VecDeque<u8>, p2: &VecDeque<u8>) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    p1.hash(&mut hasher);
+    p2.hash(&mut hasher);
+    hasher.finish()
+}
+
+fn recursive(p1: &mut VecDeque<u8>, p2: &mut VecDeque<u8>) -> bool {
+    let mut cache: HashSet<u64> = HashSet::new();
 
     while !p1.is_empty() && !p2.is_empty() {
-        if !cache.insert((p1.clone(), p2.clone())) {
+        if !cache.insert(hash(p1, p2)) {
             return true;
         }
 
         let c1 = p1.pop_front().unwrap();
         let c2 = p2.pop_front().unwrap();
 
-        let p1_wins = if p1.len() >= c1 && p2.len() >= c2 {
+        let p1_wins = if p1.len() >= c1 as usize && p2.len() >= c2 as usize {
             recursive(
-                &mut p1.iter().cloned().take(c1).collect(),
-                &mut p2.iter().cloned().take(c2).collect(),
+                &mut p1.iter().cloned().take(c1 as usize).collect(),
+                &mut p2.iter().cloned().take(c2 as usize).collect(),
             )
         } else {
             c1 > c2
